434fb9b7206115225afc3ab9752ee3ed46a10d2e
Jefferson da Silva Souza <je_jss@hotmail.com>
2020-05-14 02:40:55 -0300

    update - button exclude

--- public/scripts.js
+++ public/scripts.js
@@ -11,3 +11,31 @@ function onOff() {
                 .classList
                 .toggle('addScroll');
         }
+
+function checkFields(event) {
+
+        const valuesToCheck = [
+                'title',
+                'category',
+                'image',
+                'description',
+                'link'
+        ]
+
+        const isEmpty = valuesToCheck.find(function(value) {
+
+                let checkIfIsString = typeof event.target[value].value === 'string';
+                
+                let checkIfIsEmpty = !event.target[value].value.trim();
+
+                if (checkIfIsString && checkIfIsEmpty) {
+                        return true
+                }
+        })       
+
+        if (isEmpty) {
+                event.preventDefault();
+                alert('Por favor, necessário preencher todos os campos.');
+        }
+}
+
